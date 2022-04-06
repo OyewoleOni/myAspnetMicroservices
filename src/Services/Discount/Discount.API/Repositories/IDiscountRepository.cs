b23c8d76e78ccabb60a3e9049c01e2090d723028
Oyewole Oni <onioyewole1910@gmail.com>
2022-04-06 17:05:29 +0100

    Developing Repository Pattern Connect PostgreSQL use Dapper on Discount.API

---
+++ src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,14 @@
+﻿using Discount.API.Entities;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon discount);
+        Task<bool> UpdateDiscount(Coupon discount);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
