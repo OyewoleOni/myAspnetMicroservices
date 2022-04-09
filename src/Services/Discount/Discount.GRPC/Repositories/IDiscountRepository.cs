c021a18ced7b6fe6c33eca24e5a05f62e7229305
Oyewole Oni <onioyewole1910@gmail.com>
2022-04-09 19:24:33 +0100

    Create Discount Grpc Microservices Project in Microservices Solution And Managing PostreSQL Database Operations in Discount Grpc

---
+++ src/Services/Discount/Discount.GRPC/Repositories/IDiscountRepository.cs
@@ -0,0 +1,14 @@
+﻿using Discount.GRPC.Entities;
+using System.Threading.Tasks;
+
+namespace Discount.GRPC.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon discount);
+        Task<bool> UpdateDiscount(Coupon discount);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
