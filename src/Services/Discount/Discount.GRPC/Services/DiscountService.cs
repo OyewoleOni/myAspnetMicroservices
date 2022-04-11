e2273b7ffadb3b53d6a4337154bf788243bf7921
Oyewole Oni <onioyewole1910@gmail.com>
2022-04-11 20:16:15 +0100

    Developing DiscountService class to Implement Grpc Proto Service Methods

---
+++ src/Services/Discount/Discount.GRPC/Services/DiscountService.cs
@@ -0,0 +1,33 @@
+﻿using Discount.GRPC.Protos;
+using Discount.GRPC.Repositories;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Discount.GRPC.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository _discountRepository;
+        private readonly ILogger<DiscountService> _logger;
+        public DiscountService(IDiscountRepository discountRepository, ILogger<DiscountService> logger)
+        {
+            _discountRepository = discountRepository ?? throw new ArgumentNullException(nameof(discountRepository));
+            _logger = logger;
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _discountRepository.GetDiscount(request.ProductName);
+            if(coupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount with productName={request.ProductName} is not found"));
+            }
+
+            var couponModel = _mapper.Map<CouponModel>(coupon);
+            return couponModel;
+        }
+
+    }
+}
